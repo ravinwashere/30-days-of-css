362ae48f3373cf92e1c9e03864b5e2176338729f
Ravindra Shekhawat <shekhawat.7924@gmail.com>
2020-02-11 16:07:14 +0530

    final project

---
+++ slideshow/app.js
@@ -0,0 +1,5 @@
+document.querySelector('.banner button').addEventListener('click', () => {
+    document.querySelector('.banner').style.display = 'none';
+    document.querySelector('.slideshow-wrapper').style.cssText = 'opacity: 1; visibility: visible;';
+    document.querySelector('.slideshow').style.animation = 'slideshow 24s infinite';
+});
